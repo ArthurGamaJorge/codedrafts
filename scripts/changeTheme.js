06e67d51aeaf74d13d87937d9e9ae6433061f457
ION MATEUS NUNES OPREA <u23135@academico.cotuca.unicamp.br>
2023-09-13 09:54:12 -0300

    css

--- scripts/changeTheme.js
+++ scripts/changeTheme.js
@@ -3,15 +3,15 @@ function MudarTema(){
 
     if (button.getAttribute('src') == ImagemOriginal) {
         document.documentElement.style.setProperty('--light-shades', '#212121');
-        document.documentElement.style.setProperty('--light-accent', '#60B289');
-        document.documentElement.style.setProperty('--dark-accent', '#6DECC5');  
+        document.documentElement.style.setProperty('--light-accent', '#360303');
+        document.documentElement.style.setProperty('--dark-accent', '#e6b4b4');  
         document.documentElement.style.setProperty('--dark-shades', '#e8e8e8');
 
     button.src = "./images/LightIcon.png"
     } else {
         document.documentElement.style.setProperty('--light-shades', '#e8e8e8');
-        document.documentElement.style.setProperty('--light-accent', '#6DECC5');
-        document.documentElement.style.setProperty('--dark-accent', '#60B289');  
+        document.documentElement.style.setProperty('--light-accent', '#e6b4b4');
+        document.documentElement.style.setProperty('--dark-accent', '#360303');  
         document.documentElement.style.setProperty('--dark-shades', '#212121');
 
     button.src = ImagemOriginal
