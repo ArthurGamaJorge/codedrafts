cd0b22872f7ff9c335e9965aab538a1de3b5d6e0
ARTHUR GAMA JORGE <u23578@academico.cotuca.unicamp.br>
2023-09-19 10:57:02 -0300

    Corrigir row reverse em app

---
+++ Banco de dados/SQLQuery/StoredProceduresCRUD.sql
@@ -0,0 +1,57 @@
+CREATE OR ALTER PROCEDURE CodeDrafts.spInserirPost
+	@titulo AS nVARCHAR(100),
+	@conteudo AS nVARCHAR(4000),
+	@pontosPost AS INT = 0,
+	@dataCriacaoPost AS DATE,
+	@capa AS VARCHAR(200),
+	@aprovado AS BIT = 0,
+	@idUsuario AS INT
+AS
+BEGIN
+	INSERT INTO Post (titulo, conteudo, pontosPost, dataCriacaoPost, capa, aprovado, idUsuario)
+	VALUES (@titulo, @conteudo, @pontosPost, GetDate(), @capa, @aprovado, @idUsuario) 
+END
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spDeletarPost
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spAtualizarPost
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spInserirUsuario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spDeletarUsuario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spAtualizarUsuario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spInserirComentario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spDeletarComentario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spAtualizarComentario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spInserirComentario
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spDeletarPost
+as
+end
+
+CREATE OR ALTER PROCEDURE CodeDrafts.spAtualizarPost
+as
+end
