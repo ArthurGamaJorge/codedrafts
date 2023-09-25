f57f47e0f7d6a7f9eb0904956c03d2524a5d4c79
ARTHUR GAMA JORGE <u23578@academico.cotuca.unicamp.br>
2023-09-25 11:10:07 -0300

    iniciar criação de objetos do banco de dados

---
+++ Banco de dados/SQLQuery/dataBaseObjects.sql
@@ -0,0 +1,26 @@
+-- VIEWS
+
+
+CREATE OR ALTER VIEW CodeDrafts.V_
+
+
+
+-- ÍNDICES
+
+
+CREATE INDEX ixPost
+ON CodeDrafts.Post(titulo, capa, dataCriacaoPost) -- conteúdo é grande demais para gerar índice
+
+
+
+-- TRIGGERS
+
+CREATE OR ALTER TRIGGER
+
+
+
+-- CURSORES
+
+
+
+
