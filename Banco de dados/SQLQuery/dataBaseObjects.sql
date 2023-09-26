2b94b5e4c5a17d64e2593fab18d2e3859469f365
ARTHUR GAMA JORGE <u23578@academico.cotuca.unicamp.br>
2023-09-26 12:16:08 -0300

    Corrigir botões footer

--- Banco de dados/SQLQuery/dataBaseObjects.sql
+++ Banco de dados/SQLQuery/dataBaseObjects.sql
@@ -12,11 +12,7 @@ CREATE INDEX ixPost
 ON CodeDrafts.Post(titulo, capa, dataCriacaoPost) -- conteúdo é grande demais para gerar índice
 
 
-
 -- TRIGGERS
-
-CREATE OR ALTER TRIGGER
-
 
 
 -- CURSORES
