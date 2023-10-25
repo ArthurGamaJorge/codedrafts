a01f50bfca58ba7a2b896633e6dafbd1a9acc66e
DANIEL DORIGAN DE CARVALHO CAMPOS <u23124@academico.cotuca.unicamp.br>
2023-10-25 09:59:42 -0300

    corrigir user

--- public/scripts/scriptUser.js
+++ public/scripts/scriptUser.js
@@ -8,8 +8,4 @@ window.onload = function(){
         document.getElementById('userName').innerHTML = loginInformations.username
         document.getElementById('pontos').innerHTML = loginInformations.pontosTotais
         document.getElementById('bioText').innerText = loginInformations.descricao
-
-    document.querySelector("#boxCbPessoalRanking").appendChild(document.querySelector("#ranking").cloneNode(true))
-    document.querySelector("#containerBio").appendChild(document.querySelector("#bio").cloneNode(true))
-    document.querySelector("#boxCbCED").appendChild(document.querySelector("#cardConquistaEmDestaque").cloneNode(true))
 }
