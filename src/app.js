8a17b9003dc862df1f1c3b90709247db3a0cfffc
ION MATEUS NUNES OPREA <u23135@academico.cotuca.unicamp.br>
2023-10-31 11:43:16 -0300

    user page

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ app.post("/verificarUsuario", async(req, res) =>{
 
 app.post("/postsUser", async(req, res) =>{
   const posts = await prisma.$queryRaw
-  `select * from CodeDrafts.V_PreviewPost where idUsuario = ${req.body.idUsuario} order by pontosPost DESC`;
+  `select * from CodeDrafts.V_PreviewPost where username = ${req.body.username} order by pontosPost DESC`;
   res.json(posts)
 })
 
@@ -321,132 +321,129 @@ function createUserPage(data){
   return `
 
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="../../images/logoIconWithoutBackground.png">
-    <title>${data.nome}</title>
-
-    <link rel="stylesheet" href="../../styles/estiloUser.css">
-    <link rel="stylesheet" href="../../styles/boxes.css">
-    <link rel="stylesheet" href="../../styles/styleGenerico.css">
-
-    <script src="../../scripts/avoidFlickering.js"></script>
-    <script src="../../scripts/changeTheme.js"></script>
-    <script src="../../scripts/userSelectedButton.js"></script>
-    <script src="../../scripts/Post.js"></script>
-    
-</head>
-<body>
-
-    <button class="botao" id="btnConfigs" onclick="Editar()"><img src="../../images/settings.png"></button>
-    <a class="botao" id="btnVoltar" href="../../app.html">Voltar</a>
-
-   <header id="menuHeader">
-        <button class="headerButton" id="headerConfigs" onclick="Editar()" id="menuConfigs"><img src="../../images/settings.png"></button>
-        <a class="headerButton" id="headerVoltar" href="../../app.html">Voltar</a>
-   </header>
-
-   <div id="pageContent">
-        <div id="geral">
-            <div id="topper">
-                <div id="mainInfo">
-                    <div id="boxAvatar">
-                        <img src="${data.fotoPerfil}" id="userAvatar">
-                    </div>
-                    <div id="boxTexto">
-                        <div id="boxUserName">
-                            <p id="nomeDoUsuario">${data.nome}</p>
-                        </div>
-                        <div id="boxInfo">
-                            <p id="userName">${data.username}</p>
-                            <p id="pontos">${data.pontosTotais}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div id="center">
-                <aside id="boxConquistas">
-                    <div id="quadradoConquistas">
-
-                    </div>
-                </aside>
-
-
-                <div id="boxCbPosts">
-                    <div id="divContentButtons">
-                        <button onclick="selecionar("Posts")" id="Posts" class="contentButton">Posts</button>
-                        <button onclick="selecionar("CbPessoal")" id="CbPessoal" class="contentButton">Pessoal</button>
-                        <button onclick="selecionar("CbConquistas")" id="CbConquistas" class="contentButton">Conquistas</button>
-                    </div>
-
-                    <div id="boxPosts" class="boxCbs">
-                    </div>
-
-                    <div id="boxCbPessoal" class="boxCbs">
-                        <div id="boxCbPessoalRanking">
-                        </div>
-                        <div id="boxCbPessoalCEDBio">
-                            <div id="boxCbBIO">
-                                <div id="containerBio"></div>
-                            </div>
-                            <div id="boxCbCED">
-                            </div>
-                        </div>
-                    </div>
-
-                    <div id="boxCbConquistas" class="boxCbs">
-                            <script>document.querySelector("#boxCbConquistas").appendChild(document.querySelector("#quadradoConquistas").cloneNode(true))</script>
-                    </div>
-
-                </div>
-        
-            </div>
-        </div>    
-        
-        <aside id="side">
-            <div id="boxBIO">
-                <div id="bio">
-                    <p id="bioText">${data.descricao}</p>
-                </div>
-            </div>
-        
-            <div id="boxCED">
-                <div id="conquistaEmDestaque">
-
-                    <div class="cardConquista" id="cardConquistaEmDestaque">
-                        
-                    </div>
-
-                </div>
-            </div>
-
-            <div id="boxRANKING">
-                <div id="ranking">
-                    
-                </div>
-            </div>
-        </aside>
-           
-    </div>
-
-    <section id="box" class="confirmarDenuncia">
-        <h1>Confirmar denúncia</h1>
-        <p>Deseja denunciar o post de <a href="#">Usuário</a>? </p>
-        <button onclick="confirmarDenuncia()" id="ConfirmarButton">Confirmar</button>
-        <button onclick="fecharDenuncia()"  id="RetornarButton">Retornar</button>
-        <button id="exitLogin" onclick="fecharDenuncia()">X</button>
-</section>
-<p id="tema"></p>
-    
-
-
-</body>
-</html>
+  <!DOCTYPE html>
+  <html lang="pt-br">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="../../images/logoIconWithoutBackground.png">
+      <title>${data.nome}</title>
+  
+      <link rel="stylesheet" href="../../styles/estiloUser.css">
+      <link rel="stylesheet" href="../../styles/boxes.css">
+      <link rel="stylesheet" href="../../styles/styleGenerico.css">
+  
+      <script src="../../scripts/avoidFlickering.js"></script>
+  </head>
+  <body>
+  
+      <button class="botao" id="btnConfigs" onclick="Editar()"><img src="../../images/settings.png"></button>
+      <a class="botao" id="btnVoltar" href="../../app.html">Voltar</a>
+  
+     <header id="menuHeader">
+          <button class="headerButton" id="headerConfigs" onclick="Editar()" id="menuConfigs"><img src="../../images/settings.png"></button>
+          <a class="headerButton" id="headerVoltar" href="../../app.html">Voltar</a>
+     </header>
+  
+     <div id="pageContent">
+          <div id="geral">
+              <div id="topper">
+                  <div id="mainInfo">
+                      <div id="boxAvatar">
+                          <img src="${data.fotoPerfil}" id="userAvatar">
+                      </div>
+                      <div id="boxTexto">
+                          <div id="boxUserName">
+                              <p id="nomeDoUsuario">${data.nome}</p>
+                          </div>
+                          <div id="boxInfo">
+                              <p id="userName">${data.username}</p>
+                              <p id="pontos">${data.pontosTotais}</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              
+              <div id="center">
+                  <aside id="boxConquistas">
+                      <div id="quadradoConquistas">
+  
+                      </div>
+                  </aside>
+  
+  
+                  <div id="boxCbPosts">
+                      <div id="divContentButtons">
+                          <button onclick="selecionar("Posts")" id="Posts" class="contentButton">Posts</button>
+                          <button onclick="selecionar("CbPessoal")" id="CbPessoal" class="contentButton">Pessoal</button>
+                          <button onclick="selecionar("CbConquistas")" id="CbConquistas" class="contentButton">Conquistas</button>
+                      </div>
+  
+                      <div id="boxPosts" class="boxCbs">
+                      </div>
+  
+                      <div id="boxCbPessoal" class="boxCbs">
+                          <div id="boxCbPessoalRanking">
+                          </div>
+                          <div id="boxCbPessoalCEDBio">
+                              <div id="boxCbBIO">
+                                  <div id="containerBio"></div>
+                              </div>
+                              <div id="boxCbCED">
+                              </div>
+                          </div>
+                      </div>
+  
+                      <div id="boxCbConquistas" class="boxCbs">
+                              <script>document.querySelector("#boxCbConquistas").appendChild(document.querySelector("#quadradoConquistas").cloneNode(true))</script>
+                      </div>
+  
+                  </div>
+          
+              </div>
+          </div>        
+          <aside id="side">
+              <div id="boxBIO">
+                  <div id="bio">
+                      <p id="bioText">${data.descricao}</p>
+                  </div>
+              </div>
+          
+              <div id="boxCED">
+                  <div id="conquistaEmDestaque">
+  
+                      <div class="cardConquista" id="cardConquistaEmDestaque">
+                          
+                      </div>
+  
+                  </div>
+              </div>
+  
+              <div id="boxRANKING">
+                  <div id="ranking">
+                      
+                  </div>
+              </div>
+          </aside>
+      </div>
+  
+      <section id="box" class="confirmarDenuncia">
+          <h1>Confirmar denúncia</h1>
+          <p>Deseja denunciar o post de <a href="#">Usuário</a>? </p>
+          <button onclick="confirmarDenuncia()" id="ConfirmarButton">Confirmar</button>
+          <button onclick="fecharDenuncia()"  id="RetornarButton">Retornar</button>
+          <button id="exitLogin" onclick="fecharDenuncia()">X</button>
+  </section>
+  <p id="tema"></p>
+      
+      <script src="../../scripts/changeTheme.js"></script>
+      <script src="../../scripts/userSelectedButton.js"></script>
+      <script src="../../scripts/scriptUser.js"></script>
+      <script src="../../scripts/Post.js"></script>
+  
+  </body>
+  </html>
   
 
 
