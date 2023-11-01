54e66bb7ce900aa25462540fcfd61635f5faa13b
ION MATEUS NUNES OPREA <u23135@academico.cotuca.unicamp.br>
2023-11-01 10:41:56 -0300

    postar, fetch

--- public/scripts/Postar.js
+++ public/scripts/Postar.js
@@ -4,4 +4,20 @@ function Postar(){
     postContent = document.querySelector("#postContent").innerHTML
     capa = document.querySelector("#capa").getAttribute("src")
     alert(capa + "  "+postContent)
+
+    //info = {
+    //    titulo: loginInformations.email,
+    //    conteudo: postContent,
+    //    idUsuario: loginInformations.email,
+    //    capa: capa
+    //    //topicos: loginInformations.email
+    //}
+//
+    //fetch("/postar", {
+    //    method:"POST",
+    //    headers:{
+    //        "Content-type": "application/json"
+    //    },
+    //    body:JSON.stringify(info)
+    //})
 }
