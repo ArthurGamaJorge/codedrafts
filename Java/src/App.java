740594054b5ebe05ef6cd20dbd74581ca2fa333e
ArthurGamaJorge <arthurgamajorgetec@gmail.com>
2023-11-04 14:51:05 -0300

    Iniciar conexão banco de dados em java

--- Java/src/App.java
+++ Java/src/App.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
  
 public class App extends Application {
@@ -13,11 +21,28 @@ public class App extends Application {
     Scene scene = new Scene(root);
     
     primaryStage.setTitle("Login");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+
+    Image image = new Image("resources/icon.png");
+    primaryStage.getIcons().add(image);
+
+    primaryStage.setScene(scene);
+    primaryStage.show();
 }
  
- public static void main(String[] args) {
-        launch(args);
+ public static void main(String[] args) throws Exception{
+    
+    BufferedReader in = new BufferedReader(new FileReader("Java/src/ConnectionString.txt"));
+
+    try{
+        Connection conexão = DriverManager.getConnection(in.readLine());
+        System.out.println("Conexão com banco de dados iniciada");
+    } 
+    catch(SQLException e){
+        System.out.println("Erro na conexão com o banco de dados");
+        e.printStackTrace();
+    }
+
+    in.close();
+    launch(args);
     }
 }
--- Java/src/Controller.java
+++ Java/src/Controller.java
@@ -1,8 +1,16 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
 import javafx.event.ActionEvent;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.Node;
+
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -16,8 +24,15 @@ public class Controller {
     private PasswordField inputSenha;
 
     @FXML
-    void handleButtonAction(ActionEvent event) {
-        System.out.println("oopa");
+    void handleButtonAction(ActionEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        scene.getWindow().centerOnScreen();
+        stage.setTitle("CodeDrafts");
+        stage.show();
     }
 
 }
