1ddc1cd5aea4e94026b363d3a1544cd8996ae9ef
Daniel <cc23124@g.unicamp.br>
2023-11-16 11:36:31 -0300

    problemas com merge

---
+++ Java/FeedController.java
@@ -0,0 +1,23 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+// povoar o list
+
+public class FeedController {
+    @FXML
+    private ListView<Post> postListView;
+
+    public void initialize() {
+        // Inicialize a lista de postagens e adicione à ListView
+        ObservableList<Post> posts = FXCollections.observableArrayList(
+                new Post("Usuario1", "Conteudo da postagem 1"),
+                new Post("Usuario2", "Conteudo da postagem 2")
+                // Adicione mais postagens conforme necessário
+        );
+
+        postListView.setItems(posts);
+        postListView.setCellFactory(param -> new PostCell());
+    }
+}
--- Java/Post.java
+++ Java/Post.java
@@ -1,3 +1,17 @@
 public class Post {
-    
+    private String titulo;
+    private String conteudo;
+
+    public Post(String titulo, String conteudo){
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public String getConteudo(){
+        return this.conteudo;
+    }
 }
