3982a31da60096f8e46ee80ad6cf402aceaa63ea
ION MATEUS NUNES OPREA <u23135@academico.cotuca.unicamp.br>
2023-11-28 10:57:54 -0300

    java

--- Java/src/Conquista.java
+++ Java/src/Conquista.java
@@ -1,3 +1,7 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Conquista {
 
@@ -13,8 +17,35 @@ public class Conquista {
         this.imagem = imagem;
     }
 
+    public int getId(){return this.id;}
+    public String getNome(){return this.nome;}
+    public int getNivel(){return this.nivel;}
+    public String getImagem(){return this.imagem;}
+
+    public void setId(int newV){this.id = newV;}
+    public void setNome(String newV){this.nome = newV;}
+    public void setNivel(int newV){this.nivel = newV;}
+    public void setImagem(String newV){this.imagem = newV;}
+
+    public static List<Conquista> criarListaTopicos(ResultSet resultSet) throws SQLException {
+        List<Conquista> listaConquistas = new ArrayList<>();
+
+        while (resultSet.next()) {
+            Conquista conquista = new Conquista(
+                    resultSet.getInt("idConquista"),
+                    resultSet.getString("nome"),
+                    resultSet.getInt("nivel"),
+                    resultSet.getString("imagem")
+            );
+
+            listaConquistas.add(conquista);
+        }
+
+        return listaConquistas;
+    }
+
     public String toString(){
-        return this.id + " - " + this.nome + " - (" + this.nivel + ", "+ this.imagem +")";
+        return this.id + " - " + this.nome + " - (" + this.nivel+")";
     }
 
 }
